ba4bde3a7c2bfb8a1508cc9a2bad4a8dcb5cca5e
Ali-Peter <aliogochukwu06@gmail.com>
2025-02-22 23:55:14 +0100

    Updated app.py

--- api/app.py
+++ api/app.py
@@ -31,6 +31,10 @@ ALLOWED_EXTENSIONS = {"pdf"}
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route("/")
+def home():
+    return "Telex Automated Resume Scanner!"
+
 @app.route("/upload", methods=["POST"])
 def upload_resume():
     print("Request Headers:", request.headers)
